c84d1ccee612729f781d79b96bdb94b4e7aafc43
Lon <akarin@xevo.home>
2017-02-01 02:02:14 +0100

    time to resolve

--- sudoku.go
+++ sudoku.go
@@ -21,21 +21,23 @@ func print_grid(args []string) {
 	for i := 0; i < 9; i++ {
 		for j := 0; j < 9; j++ {
 			fmt.Printf("%c", args[i][j])
-			if (j+1)%3 == 0 && j != 8 {
+			if ((j + 1) % 3) == 0 && j != 8 {
 				fmt.Print("|")
 			}
 		}
 		fmt.Print("\n")
-		if (i+1)%3 == 0 && i != 8 {
+		if ((i + 1) % 3) == 0 && i != 8 {
 			fmt.Println("---+---+---")
 		}
 	}
 }
 
 func line_has_duplication(arg string) bool {
+	var s string
+
 	for _, chr := range arg {
 		if unicode.IsNumber(chr) {
-			s := fmt.Sprintf("%c", chr)
+			s = fmt.Sprintf("%c", chr)
 			if strings.Count(arg, s) > 1 {
 				return true
 			}
@@ -45,15 +47,17 @@ func line_has_duplication(arg string) bool {
 }
 
 func cols_have_duplication(args []string) bool {
-	var i, j int
-	var c byte
+	var i, j, y int
 
-	for i = 0; i < 8; i++ {
-		c = args[0][i]
-		if unicode.IsNumber(rune(c)) {
-			for j = 1; j < 8; j++ {
-				if args[j][i] == c {
-					return true
+	for i = 0; i < 9; i++ {
+		for j = 1; j < 9; j++ {
+			if unicode.IsNumber(rune(args[j][i])) {
+				for y = 0; y < 9; y++ {
+					if j != y {
+						if args[j][i] == args[y][i] {
+							return true
+						}
+					}
 				}
 			}
 		}
@@ -68,7 +72,7 @@ func extract_box(args []string, i int, j int) string {
 	counter = 0
 	for x := 0; x < 3; x++ {
 		for y := 0; y < 3; y++ {
-			box[counter] = args[i+y][j+x]
+			box[counter] = args[i + y][j + x]
 			counter++
 		}
 	}
@@ -77,15 +81,15 @@ func extract_box(args []string, i int, j int) string {
 
 func boxes_have_duplication(args []string) bool {
 	var i, j int
-	var box string
+//	var box string
 
 	i = 0
 	for i < 8 {
 		j = 0
 		for j < 8 {
-			box = extract_box(args, i, j)
-			//			fmt.Println(box)
-			if line_has_duplication(box) {
+//			box = extract_box(args, i, j)
+//			fmt.Println(box)
+			if line_has_duplication(extract_box(args, i, j)) {
 				return true
 			}
 			j += 3
@@ -95,16 +99,28 @@ func boxes_have_duplication(args []string) bool {
 	return false
 }
 
-func main() {
-	var args []string
+func has_minimum_required(args []string) bool {
+	var counter int
 
-	args = os.Args[1:]
-	if len(args) != 9 {
-		exit_error("Error: invalid grid")
+	for _, arg := range args {
+		for _, chr := range arg {
+			if unicode.IsNumber(rune(chr)) {
+				counter++
+			}
+		}
 	}
+	fmt.Print("\n")
+	fmt.Print(counter)
+	fmt.Println(" digits inside")
+	if counter < 17 {
+		return false
+	}
+	return true
+}
 
-	print_grid(args)
+func validate(args []string) {
 	var match bool
+
 	for _, arg := range args {
 		match, _ = regexp.MatchString("^[1-9\\.]+$", arg)
 		if match == false {
@@ -117,10 +133,30 @@ func main() {
 			exit_error("Error: there is a duplication in a line")
 		}
 	}
+	if !has_minimum_required(args) {
+		exit_error("Error: there is not enough digits in the grid")
+	}
 	if cols_have_duplication(args) {
 		exit_error("Error: there is a duplication in a column")
 	}
 	if boxes_have_duplication(args) {
 		exit_error("Error: there is a duplication in a box")
 	}
+}
+
+func resolve(args []string) {
+// go go go
+}
+
+func main() {
+	var args []string
+
+	args = os.Args[1:]
+	if len(args) != 9 {
+		exit_error("Error: invalid grid")
+	}
+
+	print_grid(args)
+	validate(args)
+	resolve(args)
 }
